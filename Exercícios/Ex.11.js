52d6ec21ffc1abc169e088586c23f3f6fd1615e7
Samuel Benjamim Mendes dos Santos <dev.samuel.mendes@gmail.com>
2025-07-21 15:13:55 -0300

    Create Ex.11.js

---
+++ Exercícios/Ex.11.js
@@ -0,0 +1,6 @@
+let larguraParade = 5;
+let alturaParade = 3;
+let areaParade = larguraParade * alturaParade;
+let tinta = areaParade / 2;
+console.log(`Sua parede tem ${larguraParade.toFixed(2)} x ${alturaParade.toFixed(2)} e sua área é de ${areaParade.toFixed(2)}m²`);
+console.log(`Para pintar essa parede, você precisará de ${tinta.toFixed(2)}L de tinta`);
